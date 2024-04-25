34f0cc5a2224631e2748b83808205c094a83e650
Ivan <kozlovnexus@gmail.com>
2024-04-25 13:18:14 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { CasePageComponent } from './сomponents/case-page/case-page.component';
+import { MainComponent } from './сomponents/main/main.component';
+import { BurgerSortComponent } from './сomponents/burger-sort/burger-sort.component';
 
 export const routes: Routes = [
-    { path: 'case', component: CasePageComponent}
+    { path: '', component: MainComponent},
+    { path: 'burger-sort', component: BurgerSortComponent},
+    { path: 'case', component: CasePageComponent},
 ];
